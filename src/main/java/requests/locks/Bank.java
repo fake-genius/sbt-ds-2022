47c0f8199e2dbb954834355af86c1435037c7d01
dawerqut <32057303+dawerqut@users.noreply.github.com>
2022-03-25 03:02:42 +0300

    hw1

--- src/main/java/requests/locks/Bank.java
+++ src/main/java/requests/locks/Bank.java
@@ -2,16 +2,24 @@ package requests.locks;
 
 import javax.persistence.*;
 
-public class BankPessimistic {
+public class Bank {
     private final Accounts accounts;
     private final EntityManagerFactory entityManagerFactory;
 
-    public BankPessimistic(Accounts accounts) {
+    public Bank(Accounts accounts) {
         this.accounts = accounts;
         this.entityManagerFactory = Persistence.createEntityManagerFactory("org.jbpm.persistence.jp");
     }
 
-    public void transfer(long fromId, long toId, double money) {
+    public void transferOptimistic(long fromId, long toId, double money) {
+        transferLock(fromId, toId, money, LockModeType.OPTIMISTIC_FORCE_INCREMENT);
+    }
+
+    public void transferPessimistic(long fromId, long toId, double money) {
+        transferLock(fromId, toId, money, LockModeType.PESSIMISTIC_FORCE_INCREMENT);
+    }
+
+    private void transferLock(long fromId, long toId, double money, LockModeType lockModeType) {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         try {
             var fromAcc = entityManager.find(Account.class, fromId);
@@ -21,10 +29,10 @@ public class BankPessimistic {
                 throw new IllegalStateException("Acc doesn't have enough money, " + money);
             }
 
-            entityManager.lock(fromAcc, LockModeType.PESSIMISTIC_FORCE_INCREMENT);
+            entityManager.lock(fromAcc, lockModeType);
             fromAcc.changeBalance(-money);
             entityManager.getTransaction().commit();
-            entityManager.lock(toAcc, LockModeType.PESSIMISTIC_FORCE_INCREMENT);
+            entityManager.lock(toAcc, lockModeType);
             toAcc.changeBalance(money);
             entityManager.getTransaction().commit();
             entityManager.close();
